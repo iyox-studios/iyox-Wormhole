cc11d87a66334c6b0474fe10584fe99b084ab990
ewuuwe <oswald.xaver@outlook.de>
2024-03-12 22:57:55 +0100

    Finish android nix build

--- flake.nix
+++ flake.nix
@@ -16,21 +16,25 @@
     nixpkgs,
     flake-utils,
     rust-overlay,
-  }:
+  }: let
+    android = {
+      versions = {
+        tools = "26.1.1";
+        platformTools = "34.0.5";
+        buildTools = ["34.0.0" "30.0.3"];
+        ndk = "23.1.7779620";
+        cmake = ["3.18.1" "3.22.1"];
+      };
+      platforms = ["34" "30" "28"];
+    };
+    pname = "iyox-wormhole";
+  in
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = import nixpkgs {
           inherit system;
           overlays = [
             (import rust-overlay)
-            /*
-            (final: prev: {
-              flutter = prev.flutter.overrideAttrs (o: {
-                patches = (o.patches or [ ]) ++ [
-                  ./my-patch.patch
-                ];
-              });})
-            */
           ];
           config = {
             android_sdk.accept_license = true;
@@ -38,46 +42,25 @@
           };
         };
 
-        pname = "iyox-wormhole";
-        version = "0.0.7";
+        version = (import ./nix/get-version.nix {inherit pkgs;}).version;
+        flutter-patched = pkgs.callPackage ./nix/flutter-patch.nix {};
 
-        flutter_fixed = pkgs.stdenv.mkDerivation {
-          name = "flutter_fixed";
+        rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./native/rust-toolchain.toml;
 
-          src = pkgs.flutter;
-
-          unpackPhase = ''
-            mkdir -p $out
-            cp -r -L --no-preserve=mode $src/* .
-            chmod +x ./bin/flutter
-          '';
-
-          phases = ["unpackPhase" "patchPhase" "buildPhase"];
-
-          patches = [
-            ./my-patch.patch
-          ];
-
-          buildInputs = [
-            pkgs.flutter
-          ];
-
-          buildPhase = ''
-            cp -r . $out/
-            cat $out/packages/flutter_tools/gradle/src/main/groovy/flutter.groovy
-          '';
-        };
-
-        rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./native/rust-toolchain.toml;
-
-        ndkVersion = "23.1.7779620";
         buildToolsVersion = "27.0.3";
+        ndkVersion = "23.1.7779620";
         androidComposition = pkgs.androidenv.composeAndroidPackages {
-          buildToolsVersions = [buildToolsVersion "30.0.3"];
-          platformVersions = ["34" "33" "32" "31" "28"];
-          abiVersions = ["armeabi-v7a" "arm64-v8a"];
+          buildToolsVersions = ["34.0.0" "30.0.3"];
+          platformVersions = ["34" "33" "32" "31" "30" "28"];
+          abiVersions = ["armeabi-v7a" "arm64-v8a" "x86" "x86_64"];
           includeNDK = true;
           ndkVersions = [ndkVersion];
+          toolsVersion = "26.1.1";
+          platformToolsVersion = "34.0.5";
+
+          includeSources = false;
+          includeSystemImages = false;
+
           cmakeVersions = ["3.18.1" "3.22.1"];
         };
         androidSdk = androidComposition.androidsdk;
@@ -87,14 +70,21 @@
           src = ./native;
           #inherit src;
           #sourceRoot = "src/native";
-          hash = "sha256-z2vmWwolBOaEP4DKMU2zw80gp9KH4F+TxpeqAJpjXxM=";
+          hash = "sha256-c3I5lMuEeXCGZJ3ZVK1POI+PBvoPKGtQjqHQ2VEZEs4=";
         };
+
+        PWD = builtins.getEnv "PWD";
       in rec {
         devShell = with pkgs;
           mkShell rec {
             ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
+            ANDROID_NDK_ROOT = "${androidSdk}/libexec/android-sdk/ndk-bundle";
+            ANDROID_HOME = "${androidSdk}/libexec/android-sdk";
+            FLUTTER_SDK = "${pkgs.flutter}";
+            GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidSdk}/libexec/android-sdk/build-tools/34.0.0/aapt2";
+            LD_LIBRARY_PATH = "${PWD}/build/linux/x64/debug/bundle/lib/:${PWD}/build/linux/x64/release/bundle/lib/:${PWD}/apps/onyx/build/linux/x64/profile/bundle/lib/";
             ANDROID_JAVA_HOME = "${pkgs.jdk.home}";
-            ANDROID_NDK = "${androidSdk}/libexec/android-sdk/ndk/${ndkVersion}";
+            #ANDROID_NDK = "${androidSdk}/libexec/android-sdk/ndk/${ndkVersion}";
             buildInputs = [
               act
               rustToolchain
@@ -130,8 +120,10 @@
 
             cargoRoot = "native";
 
-            ANDROID_HOME = "${androidSdk}/libexec/android-sdk";
-            JAVA_HOME = "${jdk.home}";
+            FLUTTER_SDK = "${pkgs.flutter}";
+            LD_LIBRARY_PATH = "./build/linux/x64/debug/bundle/lib/:./build/linux/x64/release/bundle/lib/:${PWD}/apps/onyx/build/linux/x64/profile/bundle/lib/";
+
+            ANDROID_JAVA_HOME = "${pkgs.jdk.home}";
 
             inherit cargoDeps pname version;
 
@@ -165,8 +157,13 @@
 
             cargoRoot = "native";
 
+            ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
+            ANDROID_NDK_ROOT = "${androidSdk}/libexec/android-sdk/ndk-bundle";
             ANDROID_HOME = "${androidSdk}/libexec/android-sdk";
-            JAVA_HOME = "${jdk.home}";
+            FLUTTER_SDK = "${pkgs.flutter}";
+            GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidSdk}/libexec/android-sdk/build-tools/34.0.0/aapt2";
+
+            ANDROID_JAVA_HOME = "${pkgs.jdk.home}";
 
             inherit cargoDeps pname version;
 
@@ -187,7 +184,7 @@
 
             configurePhase = ''
               echo "sdk.dir=${androidSdk}/libexec/android-sdk" >> android/local.properties
-              echo "flutter.sdk=${flutter_fixed}" >> android/local.properties
+              echo "flutter.sdk=${flutter-patched}" >> android/local.properties
 
               export HOME="$NIX_BUILD_TOP"
               flutter config --no-analytics &>/dev/null # mute first-run
@@ -206,10 +203,20 @@
               --offline --no-daemon --no-build-cache --info --full-stacktrace \
               --warning-mode=all --parallel --console=plain \
               -PnixMavenRepo=${mavenRepo} \
-              -Dorg.gradle.project.android.aapt2FromMavenOverride=${androidSdk}/libexec/android-sdk/build-tools/30.0.3/aapt2
-
+              -Dorg.gradle.project.android.aapt2FromMavenOverride=${androidSdk}/libexec/android-sdk/build-tools/34.0.0/aapt2
+              cd ..
               runHook postBuild
             '';
+
+            installPhase = ''
+              runHook preInstall
+              mkdir -p $out
+              cp -r ./build/app/outputs/ $out
+              runHook postInstall
+            '';
+
+            dontFixup = true;
+            dontStrip = true;
           };
         };
       }

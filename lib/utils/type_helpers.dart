3c40e0d5e315b35e542980c4f58621364284cb6e
Xaver Oswald <xaver.oswald@protonmail.com>
2025-03-02 15:54:28 +0100

    Create new empty project

--- lib/utils/type_helpers.dart
+++
@@ -1,8 +0,0 @@
-import '../../gen/bridge_definitions.dart';
-
-extension GetValue<T> on TUpdate {
-  T getValue() {
-    final val = value.field0;
-    return val as T;
-  }
-}
--- native/src/lib.rs
+++
@@ -1,3 +0,0 @@
-mod api;
-mod bridge_generated;
-mod wormhole;

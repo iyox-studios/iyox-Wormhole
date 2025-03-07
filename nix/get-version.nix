45af50871450011298f7981c5680d4bc4efda4a1
Xaver Oswald <ewuuwe.0@pm.me>
2025-03-07 19:48:03 +0100

    Fix regex

--- nix/get-version.nix
+++ nix/get-version.nix
@@ -1,6 +1,6 @@
 {pkgs}: let
   pubspecYaml = builtins.readFile ../pubspec.yaml;
-  versionRegex = ".+version:[[:space:]]+([0-9]+\\.+[0-9]+\\.[0-9]+)\+([0-9]+).*";
+  versionRegex = ".+version:[[:space:]]+([0-9]+\\.[0-9]+\\.[0-9]+)\\+([0-9]+).+";
   versionString = builtins.match versionRegex pubspecYaml;
 in {
   versionName = builtins.elemAt versionString 0;
